fc9c1b31f6bcd9b3585832a69614ff9bdb556ce0
Prince <prince.agarwal@anant.co.in>
2022-11-18 23:49:26 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_example/product_model.dart';
 
+//Model class
+class ProductModel {
+  String productName;
+  int price;
+  bool isSelected;
+  ProductModel(this.productName, this.price, this.isSelected);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProductModel &&
+          runtimeType == other.runtimeType &&
+          productName == other.productName;
+}
+
+//Provider class
+class ProductModelProvider extends ChangeNotifier {
+  final List<ProductModel> _productModelList = [];
+  void initialSetup() {
+    _productModelList.add(ProductModel("T-Shirt", 399, false));
+    _productModelList.add(ProductModel("Jeans", 999, false));
+    _productModelList.add(ProductModel("Socks", 99, false));
+    _productModelList.add(ProductModel("Shirt", 499, false));
+  }
+
+  void productClicked(index) {
+    _productModelList.elementAt(index).isSelected =
+        !_productModelList.elementAt(index).isSelected;
+    notifyListeners();
+  }
+
+  List<ProductModel> get productModelList => _productModelList;
+  List<ProductModel> get selectedProductList =>
+      productModelList.where((element) => element.isSelected).toList();
+}
+
+//main app starting point
 void main() {
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) => ProductModelProvider())
